76bfb5c5a084e181d78da5d153bf11d48ab97c93
MansiRaval021 <mansiraval232@gmail.com>
2026-08-06 11:19:27 +0530

    Add files via upload
    commit

---
+++ Databce/Mansi_Product/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "<h2>Mansi API</h2>";
+    echo "<ul>";
+    echo "<li><a href='Mansi/insert.php'>Insert Data</a></li>";
+    echo "<li><a href='Mansi/view.php'>View Data</a></li>";
+    echo "<li><a href='Mansi/update.php'>Update Data</a></li>";
+    echo "<li><a href='Mansi/delete.php'>Delete Data</a></li>";
+
+    echo "<li><a href='Mansi/signup.php'>Signup</a></li>";
+    echo "<li><a href='Mansi/signin.php'>Signin</a></li>";
+
+    echo "</ul>";
+
+?>
